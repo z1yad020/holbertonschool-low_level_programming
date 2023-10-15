a350bceefd424bb5d02c2aedd2ddb81a3beb700e
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-15 09:44:09 -0700

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-/**entry point for task
-*i swear that is enty point for gnu
-*/
+/* void: noting */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
