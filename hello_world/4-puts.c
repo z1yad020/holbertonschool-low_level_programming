218caac5c9b00ec118ca39cc633aead7bea30731
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-15 09:28:46 -0700

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /*entry point for task
-*i swear that is enty point for gnu
+i swear that is enty point for gnu
 */
-int main(void)
+int main(void)/*commmennnttt*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
