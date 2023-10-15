ec3bd0bf43fd42ea2625254b9e533566562409f8
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-15 10:20:39 -0700

    task 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Makes the sum of two numbers
+ * void: First operand
+ *
+ *
+ * Description: This is a longer description.
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function
+ * Return: The sum of the two parameters
+ */
+int main(void)
+{
+	printf("Size of a %s: %i bytes(s)", sizeof(char));
+	printf("Size of a %s: %i bytes(s)", sizeof(int));
+	printf("Size of a %s: %i bytes(s)", sizeof(long int));
+	printf("Size of a %s: %i bytes(s)", sizeof(long long int));
+	printf("Size of a %s: %i bytes(s)", sizeof(float));
+	return (0);
+}
