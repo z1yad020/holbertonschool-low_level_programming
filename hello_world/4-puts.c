34b825e8b40109ac7c7f61d3e9d3543a463e6e57
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-15 10:01:09 -0700

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -6,6 +6,17 @@
 
 * Return: finish of program with succed
 */
+/**
+ * op_add - Makes the sum of two numbers
+ * @arg1: First operand
+ * @arg2: Second operand
+ *
+ * Description: This is a longer description.
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function
+ * Return: The sum of the two parameters
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
