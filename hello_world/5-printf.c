0c1219407775b6f778e04254abb1995e93cdab01
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-15 10:13:42 -0700

    task 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Makes the sum of two numbers
+ * void: First operand
+ *
+ *
+ * Description: This is a longer description.
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function
+ * Return: The sum of the two parameters
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
