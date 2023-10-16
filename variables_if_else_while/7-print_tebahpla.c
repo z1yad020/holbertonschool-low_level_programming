b78821da592add0c0d9c74403a6bea13f11fc3a2
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-16 09:13:22 -0700

    task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * void: nothing
+ *
+ *
+ * Description: if else condition
+ *
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
