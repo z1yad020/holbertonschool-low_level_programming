d168e6228287f275e3c290509478c57f5851a3f9
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-16 09:07:50 -0700

    task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * void: nothing
+ *
+ *
+ * Description: if else condition
+ *
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
