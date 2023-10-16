aa70195fc074ba198755ee2e43c5f854e280f135
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-16 08:36:29 -0700

    task 2

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * void: nothing
+ *
+ *
+ * Description: if else condition
+ *
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
