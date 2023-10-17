c1896240269fd5724d6819b6f3248ea7cc1ab889
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-17 05:35:56 -0700

    task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include "main.h"
 /**
  * main - entry piont
@@ -11,5 +12,14 @@
 
 int main(void)
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
