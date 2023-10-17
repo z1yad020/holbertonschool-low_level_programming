c1673a3dd24d30af05201ae938b1d119387a3e38
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-17 07:51:35 -0700

    task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - printing if is lower case
+ *
+ * Descriptio: doing some code
+ *
+ * Return: void
+ */
+int _islower(int c)
+{
+	if (!islower(c))
+		return 0;
+	return 1;
+}
