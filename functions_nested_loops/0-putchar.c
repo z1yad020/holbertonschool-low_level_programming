e02e1b215b04db58a5b1fef26100202fe579c850
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-17 05:22:38 -0700

    task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - entry piont
+ *
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-int main()
+
+
+int main(void)
 {
-	return(0);
+	return (0);
 }
