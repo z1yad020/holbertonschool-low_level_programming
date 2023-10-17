bc9585763617fdf2d532c1fd53809e3cac4bd6af
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-17 07:19:08 -0700

    task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - printing
+ * letters
+ *
+ * Descriptio: doing some code
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i = 10;
+
+	while (i != 0)
+	{
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	i--;
+	}
+	_putchar('\n');
+}
