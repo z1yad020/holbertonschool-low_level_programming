4518027d7c866b913f9fbbb890eb48f42e2a1c44
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-17 07:57:22 -0700

    task 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isalpha - printing if is lower case
+ * @c: character
+ *
+ * Descriptio: doing some code
+ *
+ * Return: void
+ */
+int _isalpha(int c)
+{
+	if (isalpha(c))
+		return (1);
+	return (0);
+}
