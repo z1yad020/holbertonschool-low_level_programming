0a98849154198243b1a6eee3a6984803963a27c5
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-18 08:09:58 -0700

    task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,9 +14,9 @@ void times_table(void)
 {
 	int i, j;
 
-	for (int i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (int j = 0; j < 10; j++)
+		for (j = 0; j < 10; j++)
 			if (j != 9)
 				printf("%d, ", i * j);
 			else
