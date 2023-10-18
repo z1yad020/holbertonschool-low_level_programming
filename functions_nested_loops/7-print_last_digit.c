e05036b7f580b2eb7d83d76ac073bda4c40116bd
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-18 07:23:04 -0700

    task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * print_last_digit - print sign
+ * @n: number
+ *
+ * Descriptio: doing some code
+ *
+ * Return: void
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
