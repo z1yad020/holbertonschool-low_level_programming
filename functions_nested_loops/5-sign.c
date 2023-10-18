16e9763ff27fc0e08e78e78d380748be2679bbd7
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-18 06:59:39 -0700

    task 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -13,20 +13,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar('1');
-		_putchar('\n');
+		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		_putchar('1');
-		_putchar('\n');
+		return (-1);
 	}
 	else
 	{
 		_putchar('0');
-		_putchar('0');
-		_putchar('\n');
+		return (0);
 	}
-	return (0);
 }
