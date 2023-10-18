4bf913358b525429bd285af6ea2cc3fbf09febaf
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-18 07:11:10 -0700

    task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdlib>
+#include "main.h"
+#include <ctype.h>
+/**
+ * _abs - print sign
+ * @n: number
+ *
+ * Descriptio: doing some code
+ *
+ * Return: void
+ */
+int _abs(int n)
+{
+	printf("%d\n", abs(n));
+}
