a689efbe2587a9b33533a290f42999594adf76cd
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-20 09:14:35 -0700

    more loops

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _isupper - check if upper.
+ * @c: character
+ *
+ * Return: 0 or 1.
+ */
+int _isupper(int c)
+{
+	return (isupper(c));
+}
