f848cbb703650fe8d8f09d2ba1ff62ef3b39e444
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-20 09:55:32 -0700

    task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * mul - Collaboration is multiplication.
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
