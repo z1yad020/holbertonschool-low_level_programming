3f43f134ef8f3aed409140b6c100024746991fbf
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-21 09:01:55 -0700

    task 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * print_diagonal - print diagonal lines
+ * @n: line count
+ *
+ * Return: void.
+ */
+void print_diagonal(int n)
+{
+	int n = 5;
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = n; i > 0; i--)
+		{
+			for (j = n; j > 0; j--)
+			{
+				if (i == j)
+					putchar('\\');
+				else
+					putchar(' ');
+			}
+			putchar('\n');
+		}
+	}
+}
