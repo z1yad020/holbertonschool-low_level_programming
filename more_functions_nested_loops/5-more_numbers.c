1b8a59e619819c018c34196a72d889ed6bb49fa3
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-21 00:23:17 -0700

    task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * more_numbers - print number 0 to 9
+ *
+ * Return: void.
+ */
+void more_numbers(void)
+{
+	int i, j = 10;
+
+	while (--j)
+		for (i = 0; i < 15; i++)
+		{
+			if (i > 9)
+			{
+				_putchar('1');
+			}
+			_putchar(i + '0');
+		}
+}
