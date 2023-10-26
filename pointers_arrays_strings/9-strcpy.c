21e144dcd6ea502f4a80769a7f3371e0dd9e1c2d
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-26 12:10:14 -0700

    task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcpy - strcpy
+ * @dest: destination
+ * @src: source
+ *
+ * Return: pointer char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
