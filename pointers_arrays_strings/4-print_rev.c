b4856f0b90c6b178164bda78f87ed1ca54a3d47a
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-26 07:13:08 -0700

    task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - lenght of char array
+ * @s: string (char array)
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i, ln = sizeof(s) / sizeof(char);
+
+	for (i = ln - 1; i >= 0; i--)
+		_putchar(s[i]);
+}
