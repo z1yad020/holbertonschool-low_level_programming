deb4cf68f30b8ce0ba414f40eba6d7827e2824b9
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-26 09:43:26 -0700

    task 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - Half the lies they tell about me aren't true
+ * @str: string (char array)
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	while (*(str + i) != '\0')
+        i++;
+
+	while (j <= i)
+	{
+		if (j % 2 == 0)
+			putchar(*(str + j));
+		j++;
+	}
+
+	putchar('\n');
+}
