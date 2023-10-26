5109f0a88c1b9a5179f83640d9a97455194f3644
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-26 07:02:28 -0700

    task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - lenght of char array
+ * @s: string (char array)
+ *
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
