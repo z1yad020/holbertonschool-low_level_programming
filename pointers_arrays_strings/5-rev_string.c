7479de9dad7e56cfff648dbbc0ccd427869c0616
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-26 09:14:03 -0700

    task 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: string (char array)
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i = 0, j = 0;
+
+	while (*(s + i) != '\0')
+		i++;
+
+	char rs[i];
+
+	while (i-- > 0)
+		*(rs + j++) = *(s + i);
+
+	s = rs;
+}
