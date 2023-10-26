643fee97545d3a75845770445a166e00a2d35e4b
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-26 10:00:35 -0700

    task 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - Winning is only half of it. Having fun is the other half
+ * @str: string (char array)
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i = 0, n;
+
+	while (*(str + i) != '\0')
+		i++;
+
+	if (i % 2)
+		n = (i - 1) / 2;
+	else
+		n = i / 2;
+
+
+	while (n < i)
+	{
+		putchar(*(str + n));
+		n++;
+	}
+
+	putchar('\n');
+}
