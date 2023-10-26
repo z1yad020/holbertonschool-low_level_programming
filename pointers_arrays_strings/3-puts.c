3d9ada2727b3d219836c8e5521a8cc107f2384a5
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-26 07:08:51 -0700

    task 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - lenght of char array
+ * @str: string (char array)
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
