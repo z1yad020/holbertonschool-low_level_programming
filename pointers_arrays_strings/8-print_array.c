d508dde805107a8b5becc7042b24915297c62868
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-26 10:33:45 -0700

    task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - Arrays are not pointers
+ * @a: array
+ * @n: length
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++;)
+		if (i != n - 1)
+			printf("%d, ", *(a + i));
+		else
+			printf("%d", *(a + i));
+
+	putchar('\n');
+}
