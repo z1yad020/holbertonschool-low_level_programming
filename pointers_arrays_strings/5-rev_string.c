e1018f07cc8f938d2d3cd7377de3bdb5198de2db
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-26 09:32:50 -0700

    task 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -12,14 +12,15 @@
 void rev_string(char *s)
 {
 	int i = 0, x;
+	char temp;
 
 	while (*(s + i) != '\0')
 		i++;
 
 	for (x = 0; x < i / 2; x++)
 	{
-		char temp = *(s + x);
-		*(s + x) = *(s + len - x - 1);
-		*(s + len - 1 - x) = temp;
+		temp = *(s + x);
+		*(s + x) = *(s + i - x - 1);
+		*(s + i - 1 - x) = temp;
 	}
 }
