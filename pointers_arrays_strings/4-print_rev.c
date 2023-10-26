67f0b5ae4ebaaf06d1caeb43735d1725a7299f37
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-26 08:05:58 -0700

    task 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -11,12 +11,12 @@
 
 void print_rev(char *s)
 {
-	int i = 0, j = 0;
+	int i = 0;
 
-	while (*(s + i++) != '\0')
-		j++;
+	while (*(s + i) != '\0')
+		i++;
 
-	while (j >= 0)
-		_putchar(*(s + --j));
+	while (i-- > 0)
+		_putchar(*(s + i));
 
 }
