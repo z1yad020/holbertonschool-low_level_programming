20005a3b63660580a3d33906461177aaa51d28e4
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-27 10:36:24 -0700

    task 100

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,47 @@
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _atoi - integers
+ * @s: string
+ *
+ * Return: integer
+ */
+
+int _atoi(char *s)
+{
+	int i = 0, number = 0;
+
+	char sign_for_number;
+
+	while (*(s + i))
+	{
+		if (!isdigit(*(s + i)))
+		{
+			i++;
+			continue;
+		}
+
+
+		if (i != 0 && *(s + i - 1) == '-')
+			sign_for_number = '-';
+
+
+		number = number * 10 + (*(s + i) - 48);
+
+
+		if (!isdigit(*(s + i + 1)))
+		{
+			if (!(number < 0) && sign_for_number == '-')
+				number *= -1;
+
+			break;
+		}
+
+		i++;
+	}
+
+	return (number);
+}
