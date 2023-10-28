9f68583810e35175df1be49aa9f7960ed7239141
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-28 14:16:58 -0700

    task 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * cap_string - Expect the best.Prepare for the worst. Capitalize on what comes
+ * @str: string
+ *
+ * Return: string
+ */
+
+char *cap_string(char *str)
+{
+	char sprtrs[] = { ' ', ',', ';', '.',
+			  '!', '?', '\"', '(', ')',
+			  '{', '}', '\t', '\n'};
+
+	int i = 0, j;
+
+	while (*(str + i))
+	{
+		j = 0;
+		while (j < 13)
+		{
+			if (sprtrs[j] == *(str + i))
+				*(str + i + 1) = toupper(*(str + i + 1));
+			j++;
+		}
+		i++;
+	}
+
+	return (str);
+}
