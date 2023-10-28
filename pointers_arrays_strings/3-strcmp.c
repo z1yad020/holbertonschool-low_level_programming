d617a889eecfe6dff5229b21b8d993cdb00bda7d
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-28 10:51:44 -0700

    task 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -19,9 +19,9 @@ int _strcmp(char *s1, char *s2)
 		if ((*(s1 + i)) != (*(s2 + i)))
 		{
 			if (*(s1 + i) < *(s2 + i))
-				return (-1);
+				return (-13);
 			else
-				return (1);
+				return (13);
 		}
 		i++;
 	}
