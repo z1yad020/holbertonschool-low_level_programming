65f0c75b418963a5e392796c93f979725b5bbfb8
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-29 07:00:55 -0700

    task 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+#include <stdbool.h>
+
+/**
+ * _strstr - Locate substring
+ * @haystack: C string to be scanned.
+ * @needle: C string containing the sequence of characters to match.
+ *
+ * Return: a pointer to the first occurrence of str2 in str1, or a null pointer
+ * if str2 is not part of str1.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0, j = 0;
+
+	while (true)
+	{
+		i++;
+		if (*(haystack + i - 1) != *(needle + j))
+		{
+			j = 0;
+			continue;
+		}
+		j++;
+
+		if (j == 6)
+			return (haystack + i - 6);
+	}
+	return (0);
+}
