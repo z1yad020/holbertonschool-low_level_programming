d763ce7f32958c1cbd66f52cc114a869bdc8f31b
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-29 08:04:39 -0700

    task 8

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+#include <stdbool.h>
+
+/**
+ * print_diagsums - The line of life is ragged diagonal between duty and desire
+ * @a: 2D array
+ * @size: size
+ *
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, j, sumd1 = 0, sumd2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+				sumd1 += *((a + i * size) + j);
+
+			if (i + j == size - 1)
+				sumd2 += *((a + i * size) + j);
+		}
+	}
+
+	printf("%d, %d", sumd1, sumd2);
+}
