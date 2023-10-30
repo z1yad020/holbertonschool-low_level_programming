f20d7652bec9b3354166c0c8510b9cf56789379c
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-30 08:50:29 -0700

    task 1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string (char array)
+ *
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+
+	if (*s != '\0')
+		_print_rev_recursion(s + 1);
+
+	_putchar(*s);
+}
