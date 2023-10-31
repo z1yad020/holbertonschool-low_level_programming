e49d330e92c931c894aaefec3cd2ccc2d53bbf98
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-31 05:29:07 -0700

    task 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _sqrt_recursion - square root
+ * @n: number
+ *
+ * Return: root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (y < 0)
+		return (-1);
+
+	if (!y)
+		return (1);
+
+	return (_pow_recursion(x, y - 1) * x);
+}
