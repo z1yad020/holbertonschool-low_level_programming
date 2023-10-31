bf487aa0cd052c6fc3a4c60c3f3ce0e74fe5ee30
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-31 05:37:26 -0700

    task 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -3,19 +3,33 @@
 #include "main.h"
 
 /**
+ * func_for_count - another function for help
+ * @n: number
+ * @x: counter
+ *
+ * Return: result
+ */
+int func_for_count(int n, int x)
+{
+	if (x * x == n)
+		return (x);
+	else if (x * x > n)
+		return (-1);
+
+	f(n, x + 1);
+}
+
+/**
  * _sqrt_recursion - square root
  * @n: number
  *
+ *
  * Return: root
  */
-
 int _sqrt_recursion(int n)
 {
-	if (y < 0)
+	if (n < 0)
 		return (-1);
 
-	if (!y)
-		return (1);
-
-	return (_pow_recursion(x, y - 1) * x);
+	return (f(n, 0));
 }
