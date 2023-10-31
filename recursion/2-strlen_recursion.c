17909e0742abb52a3487e0dcde14ac4ca1fa5cda
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-10-31 05:14:00 -0700

    task 2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - string length
+ * @s: string (char array)
+ *
+ * Return: length
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (_strlen_recursion(s + 1) + 1);
+
+	return (1);
+
+}
