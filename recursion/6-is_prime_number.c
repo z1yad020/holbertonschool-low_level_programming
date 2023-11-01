259e80e3d23f082198d58dc9cee60090f2fe779b
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-11-01 06:54:29 -0700

    task 6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * func_for_count - another function for help
+ * @n: number
+ * @x: counter
+ *
+ * Return: result
+ */
+int func_for_count(int n, int x)
+{
+	if (x > sqrt(n) || !(n % x))
+		return (0);
+	else if (n % x)
+		return (1);
+	else
+		return (func_for_count(n, x + 1));
+}
+
+/**
+ * is_prime_number - is prime or not
+ * @n: number
+ *
+ *
+ * Return: true false
+ */
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	else if (n == 2)
+		return (1);
+	else
+		return (func_for_count(n, 2));
+}
