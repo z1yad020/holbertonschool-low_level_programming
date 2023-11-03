3ea16622a449a18a0380034a59fd6ac2ca7013f8
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-11-03 09:16:53 -0700

    task 4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - start point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) != 0)
+			sum += atoi(argv[i]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
