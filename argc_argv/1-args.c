088d7ccc332c734023ab995a2d0e9a2e9362351f
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-11-03 08:24:00 -0700

    task 1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -7,9 +7,9 @@
  *
  * Return: 0
 */
-int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
+int main(int argc,__attribute__((unused)) char *argv[])
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
