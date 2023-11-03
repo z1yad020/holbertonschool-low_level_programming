a449ce8e5302fcca04c1e9b21ae240dff8c790f7
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-11-03 09:43:00 -0700

    task 4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,33 +1,62 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
 
 /**
- * main - start point
- * @argc: argument count
- * @argv: argument vector
+ * check_num - check the code
  *
- * Return: 0
-*/
+ * @s: var
+ *
+ * Return: Always 0.
+ */
+int check_num(char *s)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(s))
+
+	{
+		if (!isdigit(s[count]))
+		{
+			return (0);
+		}
+
+		count++;
+	}
+	return (1);
+}
+
+
+
+/**
+ * main - check the code
+ *
+ * @argc: var
+ * @argv: var
+ *
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int result = 0, i;
 
-	if (argc == 1)
+	for (i = 1; i <= argc - 1; i++)
 	{
-		printf("0\n");
-		return (0);
-	}
-
-	for (i = 1; i < argc; i++)
-	{
-		if (atoi(argv[i]) != 0)
-			sum += atoi(argv[i]);
+		if (check_num(argv[i]))
+		{
+			result = result + atoi(argv[i]);
+		}
 		else
 		{
 			printf("Error\n");
 			return (1);
 		}
 	}
-	printf("%d\n", sum);
+
+	printf("%d\n", result);
 	return (0);
 }
