261f60e8f9aaab58a642e4dc49ecb95fbfbc67a6
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-11-03 08:22:21 -0700

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - start point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+*/
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
