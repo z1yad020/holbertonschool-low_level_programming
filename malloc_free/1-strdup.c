fe2c15f3d21219913b4d80291846f1c91ab3a331
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-11-14 10:35:28 -0800

    task 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - The woman who has no imagination has no wings
+ * @str: string
+ *
+ * Return: new loc to dublicated string
+ */
+char *_strdup(char *str)
+{
+	char *arr;
+	unsigned int i = 0, j;
+
+	if (!str)
+		return (NULL);
+
+
+	while (1)
+	{
+		if (!(*(str + i)))
+			break;
+		i++;
+	}
+
+	arr = (char *)malloc(i * sizeof(*str));
+
+	if (!arr)
+		return (NULL);
+
+	for (j = 0; j < i; j++)
+		*(arr + j) = *(str + j);
+
+	return (arr);
+
+}
