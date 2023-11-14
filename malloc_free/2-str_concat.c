09f45c004fd71f1c39eed1034a0da90ff63376ea
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-11-14 11:06:47 -0800

    task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,18 +17,18 @@ char *str_concat(char *s1, char *s2)
 	unsigned int s1i = 0, s2i = 0, s3i, i;
 
 	if (!s1)
-		s1 = "";
+		*s1 = "";
 
 	if (!s2)
-                s2 = "";
+		*s2 = "";
 
 	while (1)
 		if (!(*(s1 + s1i++)))
 			break;
 
 	while (1)
-                if (!(*(s2 + s2i++)))
-                        break;
+		if (!(*(s2 + s2i++)))
+			break;
 
 	s3i = s1i + s2i - 1;
 
@@ -42,7 +42,7 @@ char *str_concat(char *s1, char *s2)
 		*(s3 + i) = *(s1 + i);
 
 	for (; i < s3i; i++)
-                *(s3 + i) = *(s2 + i);
+		*(s3 + i) = *(s2 + i);
 
 	return (s3);
 
