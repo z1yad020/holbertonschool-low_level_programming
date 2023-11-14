33adab70916a5e29bd4557f0d5ba421df4983610
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-11-14 11:03:33 -0800

    task 2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - He who is not courageous enough to take risks
+ * will accomplish nothing in life
+ * @s1: string
+ * @s2: string
+ *
+ * Return: concatenated string loc
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *s3;
+	unsigned int s1i = 0, s2i = 0, s3i, i;
+
+	if (!s1)
+		s1 = "";
+
+	if (!s2)
+                s2 = "";
+
+	while (1)
+		if (!(*(s1 + s1i++)))
+			break;
+
+	while (1)
+                if (!(*(s2 + s2i++)))
+                        break;
+
+	s3i = s1i + s2i - 1;
+
+	s3 = (char *)malloc(s3i * sizeof(*s1));
+
+	if (!s3)
+		return (NULL);
+
+
+	for (i = 0; i < s1i; i++)
+		*(s3 + i) = *(s1 + i);
+
+	for (; i < s3i; i++)
+                *(s3 + i) = *(s2 + i);
+
+	return (s3);
+
+}
