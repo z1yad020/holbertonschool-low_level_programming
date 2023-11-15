cde4656155cdca614a467d3f12f5d8fa23d1cc59
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-11-15 12:26:42 -0800

    task 4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - It's not bragging if you can back it up
+ * @grid: 2D Array
+ * @height: height
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	while (height >= 0)
+	{
+		free(*(grid + height));
+		height--;
+	}
+	free(grid);
+}
