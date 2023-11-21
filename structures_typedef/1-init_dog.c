8a900542787a2b74799e29d5f4e457d8a56652d0
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-11-21 11:54:28 -0800

    task 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+/**
+ * init_dog - A dog is the only thing on earth that loves you more than
+ * you love yourself
+ * @d: dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
