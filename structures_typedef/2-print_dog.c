e9089489e3855562b7360c5b7fc220f372443b60
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-11-22 00:28:46 -0800

    task 3

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -16,17 +16,14 @@ void print_dog(struct dog *d)
 	if (!(d->name) || !(d->age) || !(d->owner))
 	{
 		if (!(d->name))
-			printf("Name: (nil)");
+			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
 
-		if (!(d->age))
-			printf("Age: (nil)");
-		else
-			printf("Age: %f\n", d->age);
+		printf("Age: %f\n", d->age);
 
 		if (!(d->owner))
-			printf("Owner: (nil)");
+			printf("Owner: (nil)\n");
 		else
 			printf("Owner: %s\n", d->owner);
 
