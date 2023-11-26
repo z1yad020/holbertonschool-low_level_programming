5f0ad0ffd278966f9e701b2d437d0076ab036927
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-11-26 07:02:22 -0800

    task 2

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - To be is to be the value of a variable
+ * @n: size of args
+ * @separator: separator
+ *
+ * Return: printing number
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	int sum = 0;
+	va_list ap;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+
+		if (i != (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+}
