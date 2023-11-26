0db4fa5c330881309cc527db2fb8320e724e5539
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-11-26 10:30:37 -0800

    task 4

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,54 @@
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_all - To be is a to be the value of a variable
+ * @format: list of types of arguments passed to the function
+ *
+ * Return: printing everything
+ */
+void print_all(const char * const format, ...)
+{
+	const size_t size = strlen(format), size_t i = 0;
+	char *formatcp = malloc(size), *str;
+	va_list ap;
+
+	strcpy(formatcp, format);
+	va_start(ap, format);
+
+	while (i < size)
+	{
+		switch (*(formatcp++))
+		{
+		case 'c':
+			printf("%c", va_arg(ap, int));
+			break;
+		case 'i':
+			printf("%i", va_arg(ap, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(ap, double));
+			break;
+		case 's':
+			str = va_arg(ap, char*);
+			if (!str)
+			{
+				printf("(nil)");
+				break;
+			}
+
+			printf("%s", str);
+			break;
+		default:
+			i++;
+			continue;
+		}
+		if (i != (size - 1))
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	free(formatcp);
+}
