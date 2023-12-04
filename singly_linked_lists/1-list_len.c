26abcf4127d5160d103a6ae2dd3887a27bbe81c6
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-12-04 11:50:02 -0800

    task 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_list - print size of linked list
+ * @h: pointer to list (head)
+ *
+ * Return: size of list
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+	const list_t *node = h;
+
+	while (node)
+	{
+		node = node->next;
+		count++;
+	}
+	return (count);
+}
