9e28c101ffef96b83f6e48738d6e5dad8d73362b
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-12-04 23:47:37 -0800

    Protoypes and Readme

---
+++ doubly_linked_lists/README.md
@@ -0,0 +1,11 @@
+C - Doubly linked lists
+
+		Print list
+		List length
+		Add node
+		Add node at the end
+		Free list
+		Get node at index
+		Sum list
+		Insert at index
+		Delete at index
---
+++ doubly_linked_lists/lists.h
@@ -0,0 +1,36 @@
+#ifndef __FILE_NAME__
+#define __FILE_NAME__
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+typedef struct slistint_s *dnodePtr;
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ *
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+
+size_t print_dlistint(const dlistint_t *);
+size_t dlistint_len(const dlistint_t *);
+dlistint_t *add_dnodeint(dlistint_t **, const int);
+void free_dlistint(dlistint_t *);
+dlistint_t *get_dnodeint_at_index(dlistint_t *, unsigned int);
+int sum_dlistint(dlistint_t *);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **, unsigned int, int);
+int delete_dnodeint_at_index(dlistint_t **, unsigned int);
+
+#endif
