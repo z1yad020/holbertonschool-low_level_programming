aaea1884a0d655234897ebb6a3361c8f17824170
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-12-05 02:21:16 -0800

    task 0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print double linked list
+ * @h: pointer to list (head)
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count = 0;
+	const dlistint_t *node = h;
+
+	while (node)
+	{
+		printf(node->n);
+		node = node->next;
+		count++;
+	}
+	return (count);
+}
