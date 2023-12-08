3f3957e91ecf9c141845cf7102640864f77f0cee
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-12-08 06:59:13 -0800

    task 8

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -68,31 +68,3 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	return (current);
 }
-
-/**
- * add_dnodeint - adds a new node at the beginning of a linked list
- * @head: pointer to pointer that point head)
- * @n: int
- * Return: the address of the new element, or NULL if it failed
- */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
-{
-	dnodePtr newNode;
-
-	if (!head)
-		return (NULL);
-
-	newNode = malloc(sizeof(dlistint_t));
-	if (!newNode)
-		return (NULL);
-
-	newNode->n = n;
-	newNode->next = *head;
-	newNode->prev = NULL;
-
-	if (*head)
-		(*head)->prev = newNode;
-	*head = newNode;
-
-	return (newNode);
-}
