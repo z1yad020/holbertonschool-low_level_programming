e8778753fe2acba5de76e6fa566616173640ec00
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-12-08 11:40:25 -0800

    task 9

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -16,7 +16,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (!index)
 	{
 		*head = remNode->next;
-		*head->prev = NULL;
+		(*head)->prev = NULL;
 		free(remNode);
 		return (1);
 	}
