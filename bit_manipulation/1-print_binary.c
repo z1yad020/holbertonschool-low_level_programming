3309d502068edb3e34d4adbe3af222d4155e2d1d
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-12-10 06:04:42 -0800

    task 1

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number in decimal
+ * Return: void
+ */
+void print_binary(unsigned long int n);
+{
+	int i = 10;
+	short chck = 0;
+	unsigned int x;
+
+	if (!n)
+	{
+		putchar('0');
+		return;
+	}
+
+	while (i--)
+	{
+		x = (n >> i) & 1;
+
+		if (!chck && !x)
+			continue;
+		else
+			chck = 1;
+
+		putchar('0' + x);
+	}
+}
