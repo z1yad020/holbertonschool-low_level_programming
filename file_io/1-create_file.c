5ee4c6b6419151de3c5ad9c3fb6a03ba9c7dfa35
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-12-12 15:36:28 -0800

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 		goto finish;
 
 	if (text_content)
-		lentext = write(fd, text_content, strlen(text_content) + 1);
+		lentext = write(fd, text_content, strlen(text_content));
 	else
 		lentext = write(fd, text_content, 0);
 
