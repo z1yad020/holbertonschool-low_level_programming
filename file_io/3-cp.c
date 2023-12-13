60b28f0ffad79bf6e9ce24b11298ed0818199e71
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-12-13 05:52:39 -0800

    task 4

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -23,27 +23,28 @@ int main(int argc, char **argv)
 		write(STDERR_FILENO, "Usage: cp file_from file_to\n", 28);
 		exit(97);
 	}
-	fd_ffrom = open(*argv[1], O_RDONLY);
-	fd_fto = open(*argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	fd_ffrom = open(argv[1], O_RDONLY);
+	fd_fto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
 	readtext = read(fd_ffrom, buff, 1024);
 	writetext = write(fd_fto, buff, readtext);
 	if (fd_ffrom < 0 || readtext < 0)
 	{
 		free(buff);
 		dprintf(STDERR_FILENO,
-			"Error: Can't read from file %s\n", *argv[1]);
+			"Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (fd_fto < 0 || writetext < 0)
 	{
 		free(buff);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", *argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	if (close(fd_ffrom))
 	{
 		free(buff);
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_ffrom);
 		exit(100);
 	}
 	if (close(fd_fto))
