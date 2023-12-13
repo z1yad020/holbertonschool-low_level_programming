ffac1d3e63c8819125edb9f479489d2ee3086ab0
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-12-13 07:06:09 -0800

    task 4

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,6 +6,40 @@
 #include "main.h"
 
 /**
+ * exitstatus - exit checker
+ * @fd_ffrom: fd file from
+ * @fd_fto: fd file to
+ * @readtext: read count
+ * @writetext: wrote count
+ * @argv: argument vector
+ * Return: void
+ */
+void exitstatus(char **argv,
+		int fd_ffrom, int fd_fto, int readtext, int writetext)
+{
+	if (fd_ffrom < 0 || readtext < 0)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	if (fd_fto < 0 || writetext < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	if (close(fd_ffrom))
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_ffrom);
+
+	if (close(fd_fto))
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_fto);
+
+	exit(100);
+}
+
+/**
  * main - copies the content of a file to another file (entry point)
  * @argc: argument count
  * @argv: argument vector
@@ -14,45 +48,23 @@
  */
 int main(int argc, char **argv)
 {
-	int fd_ffrom, fd_fto, readtext, writetext;
-	char *buff = calloc(1024, sizeof(char));
+	int fd_ffrom, fd_fto, readtext = 1024, writetext;
+	char buff[1024];
 
 	if (argc != 3)
 	{
-		free(buff);
 		write(STDERR_FILENO, "Usage: cp file_from file_to\n", 28);
 		exit(97);
 	}
 	fd_ffrom = open(argv[1], O_RDONLY);
-	fd_fto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	fd_fto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 
-	readtext = read(fd_ffrom, buff, 1024);
-	writetext = write(fd_fto, buff, readtext);
-	if (fd_ffrom < 0 || readtext < 0)
+	while (readtext == 1024)
 	{
-		free(buff);
-		dprintf(STDERR_FILENO,
-			"Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		readtext = read(fd_ffrom, buff, 1024);
+		writetext = write(fd_fto, buff, readtext);
+		exitstatus(argv, fd_ffrom, fd_fto, readtext, writetext);
 	}
-	if (fd_fto < 0 || writetext < 0)
-	{
-		free(buff);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	if (close(fd_ffrom))
-	{
-		free(buff);
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_ffrom);
-		exit(100);
-	}
-	if (close(fd_fto))
-	{
-		free(buff);
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_fto);
-		exit(100);
-	}
-	free(buff);
+
 	return (0);
 }
