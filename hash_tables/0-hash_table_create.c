4031df6796275a738669d418cdcb9d59c36d3ff2
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-12-16 12:48:14 -0800

    task 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -14,10 +14,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ht = malloc(sizeof(hash_table_t));
 	size_t i = 0;
 
+	if (!ht)
+		return (NULL);
+
 	ht->size = size;
 	ht->array = malloc(size * sizeof(hash_node_t *));
 
-	if (!ht || !ht->array)
+	if (!ht->array)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
