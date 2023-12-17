c59f084afb5d69b29a33bdc3ec3cee2a65ab7706
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-12-17 08:44:47 -0800

    task 7

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * dltlist - delete hash node list
+ * @tmp: temp hash node
+ * Return: void
+ */
+void dltlist(hash_node_t *tmp)
+{
+	if (tmp->next)
+		dltlist(tmp->next);
+
+	free(tmp->key);
+	free(tmp->value);
+	free(tmp);
+}
+
+/**
+ * hash_table_delete - delete a hash table
+ * @ht: hash table
+ *
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *tmp;
+	unsigned long int node_idx;
+
+	if (!ht)
+		return;
+
+	for (node_idx = 0; node_idx < ht->size; node_idx++)
+	{
+		if (ht->array[node_idx])
+			dltlist(ht->array[node_idx]);
+		free(ht->array[node_idx]);
+	}
+	free(ht);
+}
