c5d295c69ea6b79bfa43e762a246348caa725e6b
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-12-17 06:37:41 -0800

    task 4

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,17 +13,32 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node = malloc(sizeof(hash_node_t));
+	hash_node_t *node, *tmp;
 	unsigned long int node_idx;
 
-	if (!node || !strlen(key))
-	{
-		free(node);
+	if (!ht || !strlen(key))
 		return (0);
-	}
 
 	node_idx = key_index((unsigned char *)key, ht->size);
 
+	tmp = ht->array[node_idx];
+	while (tmp)
+	{
+		if (strcmp(key, tmp->key))
+		{
+			tmp = tmp->next;
+			continue;
+		}
+		free(tmp->value);
+		tmp->value = strdup(value);
+		if (!tmp->value)
+			return (0);
+		return (1);
+	}
+
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (0);
 	node->key = strdup(key);
 	node->value = strdup(value);
 	if (!node->key || !node->value)
@@ -33,7 +48,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(node->value);
 		return (0);
 	}
-
 	node->next = ht->array[node_idx];
 	ht->array[node_idx] = node;
 	return (1);
