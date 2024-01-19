bce383f1f35284b3f9df9a60805a72406d3ab132
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-01-19 06:33:53 -0800

    linear search

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef __FILE_NAME__
+#define __FILE_NAME__
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int linear_search(int *, size_t, int);
+int binary_search(int *, size_t, int);
+
+#endif
+
